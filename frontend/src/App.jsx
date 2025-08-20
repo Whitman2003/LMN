b5ed9cd8b5a8dddfc551c27cdc6287073ebbc394
Aidan Whitman <whitmanaidan@yahoo.com>
2025-08-19 19:57:05 -0500

    Completely Set up the Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container text-center mt-5'>
+      <h1 className="text-primary">Welcome to Vite + React</h1>
+      <button className='btn btn-success'>Click Me</button>
+    </div>
+  )
+}
+
+export default App
